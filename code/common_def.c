2faa9b45a8cb9c2d6920cac968bf7bfe60b1ef30
Bill Noble <winobes@gmail.com>
2013-01-06 16:33:22 -0500

    simplified background variables

--- code/common_def.c
+++ code/common_def.c
@@ -63,7 +63,7 @@ typedef struct {
 
 struct Background {
 	float parallax_rate;
-	int parallax_x1, parallax_x2, parallax_y1, parallax_y2;
+	int x1, x2, y1, y2;
 	ALLEGRO_BITMAP *background_image;
 };
 
--- code/update_background.c
+++ code/update_background.c
@@ -3,22 +3,22 @@ void update_background(struct Background *background, float x, float y, int w, i
 	float parallax_counter = x;
 	bool parallax_switch = false;
 
-	background->parallax_x1 = 0;
-	background->parallax_x2 = -1;	
-	background->parallax_y1 = -1;
-	background->parallax_y2 = 0;
+	background->x1 = 0;
+	background->x2 = -1;	
+	background->y1 = -1;
+	background->y2 = 0;
 
 	while (parallax_counter > w/2) {
 		
 		parallax_counter -= width/background->parallax_rate;
 
 		if (parallax_switch) {
-			background->parallax_x1 += 2;
+			background->x1 += 2;
 			parallax_switch = false;
 		}
 
 		else {
-			background->parallax_x2 += 2;
+			background->x2 += 2;
 			parallax_switch = true;
 		}
 
@@ -29,12 +29,12 @@ void update_background(struct Background *background, float x, float y, int w, i
 		parallax_counter += width/background->parallax_rate;
 
 		if(parallax_switch) {
-			background->parallax_x2 -= 2;
+			background->x2 -= 2;
 			parallax_switch = false;
 		}
 
 		else {
-			background->parallax_x1 -= 2;
+			background->x1 -= 2;
 			parallax_switch = true;
 		}
 
@@ -49,12 +49,12 @@ void update_background(struct Background *background, float x, float y, int w, i
 		parallax_counter -= height/background->parallax_rate;
 
 		if (parallax_switch) {
-			background->parallax_y2 += 2;
+			background->y2 += 2;
 			parallax_switch = false;
 		}
 
 		else {
-			background->parallax_y1 += 2;
+			background->y1 += 2;
 			parallax_switch = true;
 		}
 
@@ -65,12 +65,12 @@ void update_background(struct Background *background, float x, float y, int w, i
 		parallax_counter += height/background->parallax_rate;
 
 		if(parallax_switch) {
-			background->parallax_y1 -= 2;
+			background->y1 -= 2;
 			parallax_switch = false;
 		}
 
 		else {
-			background->parallax_y2 -= 2;
+			background->y2 -= 2;
 			parallax_switch = true;
 		}
 
