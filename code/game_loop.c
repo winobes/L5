cfec23d035e2a394dcd01964c8b9c1f0f9f9cb29
rcplane <bennettwin55@gmail.com>
2013-01-06 12:26:38 -0500

    compile with debugging, trying free statements

--- code/game_loop.c
+++ code/game_loop.c
@@ -40,5 +40,61 @@ int game_loop() {
 
 	}
 
+	// free everything we ever did malloc // TODO make individual teardown functions
+	int i, j, k;
+
+	// player
+	for (j = 0; j < gs->player->ext.nverts; j++) {
+		free(gs->player->ext.vert[j]);
+	}
+	free(gs->player->ext.vert);
+	free(gs->player->ani);
+	free(gs->player->aniflags);
+	free(gs->player->weapon.sprite);
+	free(gs->player->weapon.d);
+	free(gs->player->weapon.x);
+	free(gs->player->weapon.y);
+	free(gs->player->weapon.dx);
+	free(gs->player->weapon.dy);
+	free(gs->player->weapon.source_x);
+	free(gs->player->weapon.timer);
+	free(gs->player->weapon.exists);
+	for (j = 0; j < gs->player->weapon.ext.nverts; j++) {
+		free(gs->player->weapon.ext.vert[j]);
+	}
+	free(gs->player->weapon.ext.vert);
+	free(gs->player);
+	
+	// rooms
+	for (i = 0; i < gs->nrooms; i++) {
+		for (j = 0; j < gs->room[i]->nwalls; j++) {
+			for (k = 0; k < gs->room[i]->wall[j]->ext.nverts; k++) {
+				free(gs->room[i]->wall[j]->ext.vert[k]);
+			}
+			free(gs->room[i]->wall[j]->ext.vert);
+		}
+		for (j = 0; j < gs->room[i]->nwalls; j++) {
+			free(gs->room[i]->wall[j]);
+		}
+		free(gs->room[i]->wall);
+		free(gs->room[i]->background);
+		free(gs->room[i]);
+	}
+	free(gs->room);
+
+	// npcs
+	for (i = 0; i < gs->nnpcs; i++) {
+		free(gs->npc[i].keys);
+		for (j = 0; j < gs->npc[0].ext.nverts; j++) {
+			free(gs->npc[i].ext.vert[j]);
+		}
+		free(gs->npc[i].ext.vert);
+		free(gs->npc[i].ani);
+	}
+	free(gs->npc);
+
+	// GameState
+	free(gs);	
+
 	return 0;
 }
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 all: code/main.c
-	gcc -Wall -o a code/main.c -lm `pkg-config --cflags --libs allegro-5.0 allegro_image-5.0 allegro_font-5.0 allegro_ttf-5.0` -O3
+	gcc -Wall -o a code/main.c -g -lm `pkg-config --cflags --libs allegro-5.0 allegro_image-5.0 allegro_font-5.0 allegro_ttf-5.0` -O3
